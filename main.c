f340487925c6287b07e15d10c4e72cb2894f2753
Ben Tatman <ben@tatmans.co.uk>
2019-07-28 20:17:39 +0100

    Correct indentation

--- main.c
+++ main.c
@@ -13,8 +13,8 @@ typedef struct {
 typedef struct {
 	obj objects[50];
 	int n_objects;
-    int print, step;
-    float dt;
+	int print, step;
+	float dt;
 } sys;
 
 v_3 force_v(obj a, obj b) {
@@ -50,7 +50,7 @@ int gravity_v(float dt, sys *syst, int n) {
 		resultant = add_3(resultant, force);
 	}
 	
-    
+	
 	syst->objects[n].vel = add_3(syst->objects[n].vel, resultant);
 	return 1;
 }
@@ -60,29 +60,29 @@ int iterate_pos_v(float dt, sys *syst, int n) {
 	v_3 v_dt = s_mul_3(dt, syst->objects[n].vel);
 
 	syst->objects[n].pos = add_3(syst->objects[n].pos, v_dt);
-    return 1;
+	return 1;
 }
 
 int read_input(char * filename, sys *syst) {
-    printf("%s\n", filename);
-    FILE * fp;
-    fp = fopen(filename, "r");
-    int n = 0;
-    fscanf(fp, "%d:%d:%f", &syst->step, &syst->print, &syst->dt);
-    while (fscanf(fp, "%f,%f,%f,%f,%f,%f,%f", 
-        &syst->objects[n].pos.v[0],
-        &syst->objects[n].pos.v[1],
-        &syst->objects[n].pos.v[2],
-        &syst->objects[n].vel.v[0],
-        &syst->objects[n].vel.v[1],
-        &syst->objects[n].vel.v[2],
-        &syst->objects[n].m) != EOF) 
-    {
-        n++;
-    }
-    syst->n_objects = n;
-    printf("%d\n", n);
-    fclose(fp);
+	printf("%s\n", filename);
+	FILE * fp;
+	fp = fopen(filename, "r");
+	int n = 0;
+	fscanf(fp, "%d:%d:%f", &syst->step, &syst->print, &syst->dt);
+	while (fscanf(fp, "%f,%f,%f,%f,%f,%f,%f", 
+		&syst->objects[n].pos.v[0],
+		&syst->objects[n].pos.v[1],
+		&syst->objects[n].pos.v[2],
+		&syst->objects[n].vel.v[0],
+		&syst->objects[n].vel.v[1],
+		&syst->objects[n].vel.v[2],
+		&syst->objects[n].m) != EOF) 
+	{
+		n++;
+	}
+	syst->n_objects = n;
+	printf("%d\n", n);
+	fclose(fp);
 }
 
 int main(int argc, char*argv[]) {
@@ -101,21 +101,21 @@ int main(int argc, char*argv[]) {
 	FILE * outputs[50];
 	char fname[50];
 	char base_name[50] = "";
-    char input_name[50];
+	char input_name[50];
 
-    if (argc == 1) {
-        printf("Please enter filename\n");
-        exit(0);
-    }
-    strcpy(input_name, argv[1]);
-    if (argc > 2)
-        strcpy(base_name, argv[2]);
-    if (read_input(input_name, &syst) == -1) {
-        printf("Error in reading file\n");
-        exit(0);
-    }
-    
-    
+	if (argc == 1) {
+		printf("Please enter filename\n");
+		exit(0);
+	}
+	strcpy(input_name, argv[1]);
+	if (argc > 2)
+		strcpy(base_name, argv[2]);
+	if (read_input(input_name, &syst) == -1) {
+		printf("Error in reading file\n");
+		exit(0);
+	}
+	
+	
 	for (t = 0; t < syst.n_objects; t++) {
 		sprintf(fname, "%s-%d.csv", base_name, t);
 		outputs[t] = fopen(fname, "w");
@@ -127,22 +127,22 @@ int main(int argc, char*argv[]) {
 	char in[50];
 	while (strcmp(in, "exit") != 0) {
 		scanf("%s", in);
-        for (t = 0; t < syst.step; t++) {
-            for (n = 0; n < syst.n_objects; n++) {
-                if (t % syst.print == 0) {
-                    fprintf(outputs[n], "%f, ", t*syst.dt);
-                    f_print_3(&outputs[n], syst.objects[n].pos);
-                }
-                gravity_v(syst.dt, &syst, n);
-                
-            }
-            for (n = 0; n < syst.n_objects; n++)
-                iterate_pos_v(syst.dt, &syst, n);
-            
-        }
+		for (t = 0; t < syst.step; t++) {
+			for (n = 0; n < syst.n_objects; n++) {
+				if (t % syst.print == 0) {
+					fprintf(outputs[n], "%f, ", t*syst.dt);
+					f_print_3(&outputs[n], syst.objects[n].pos);
+				}
+				gravity_v(syst.dt, &syst, n);
+				
+			}
+			for (n = 0; n < syst.n_objects; n++)
+				iterate_pos_v(syst.dt, &syst, n);
+			
+		}
 	}
 	for (t = 0; t < syst.n_objects; t++) {
-        fclose(outputs[t]);
-    }
+		fclose(outputs[t]);
+	}
 	return 1;
 }
